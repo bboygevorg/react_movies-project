8a4aa2e99e23a6d2241d6c52183eec93496df019
Gevorg <bboygevor@gmail.com>
2022-03-05 17:03:46 +0300

    project complate

---
+++ src/layout/header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="indigo lighten-3">
+        <div className="nav-wrapper">
+            <a href="#!" className="brand-logo">React Movies</a>
+            <ul className="right hide-on-med-and-down">
+                <li><a href="!#">Sass</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
